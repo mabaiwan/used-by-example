e017110ce64e2eb9cc36111e51a5af0967ace381
chen.ma <machencoding@163.com>
2021-07-16 08:42:40 +0800

    first commit

---
+++ src/main/java/com/github/baiwan/usedbyexample/UsedByExampleApplication.java
@@ -0,0 +1,13 @@
+package com.github.baiwan.usedbyexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsedByExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UsedByExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/baiwan/usedbyexample/UsedByExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.baiwan.usedbyexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsedByExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
